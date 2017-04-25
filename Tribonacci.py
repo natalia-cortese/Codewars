8e58cadc433400d24596beabe265ed11b3190bc5
Josh Lawman <jlawman@gmail.com>
2017-04-24 17:20:53 -0700

    Tribonacci script was not saved in correct file.

--- Tribonacci.py
+++ Tribonacci.py
@@ -4,4 +4,7 @@ a signature array/list, returns the first n elements
 - signature included of the so seeded sequence.
 """
 def tribonacci(signature,n):
-    #your code here
+    sequence=signature #Begin with signature (first 3 elmts)
+    for i in range(2,n-1):
+    	sequence.append(sequence[i-2]+sequence[i-1]+sequence[i])
+    return sequence[0:n]
