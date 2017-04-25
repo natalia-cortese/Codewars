b28dcffebf7c39fbcdd80e1fae5c6b17fb036fb9
Josh Lawman <jlawman@gmail.com>
2017-04-24 17:04:46 -0700

    Printer Errors Script

---
+++ Printer Errors.py
@@ -0,0 +1,11 @@
+def printer_error(s):
+    goodLetters='abcdefghijklm'
+
+    errors=0
+    totalLetters=len(s)
+
+    for letter in s:
+        if letter not in goodLetters:
+            errors+=1
+
+    return str(errors) + "/" + str(totalLetters)
