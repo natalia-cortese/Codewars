d945d8a5bb4e514ed0e9899b8a3f5438d379f1aa
Josh Lawman <jlawman@gmail.com>
2017-04-24 17:19:01 -0700

    Tribonacci

--- Printer Errors.py
+++ Printer Errors.py
@@ -1,3 +1,9 @@
+"""Challenge: You have to write a function printer_error
+which given a string will output the error rate
+of the printer as a string representing a rational
+whose numerator is the number of errors and the denominator
+the length of the control string."""
+
 def printer_error(s):
     goodLetters='abcdefghijklm'
 
---
+++ Tribonacci.py
@@ -0,0 +1,7 @@
+""" 
+Challenge: You need to create a fibonacci function that given 
+a signature array/list, returns the first n elements 
+- signature included of the so seeded sequence.
+"""
+def tribonacci(signature,n):
+    #your code here
