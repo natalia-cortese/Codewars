77611aab86829ad5f990d16561003979c3ebc92d
Josh Lawman <jlawman@gmail.com>
2017-04-25 14:19:17 -0700

    Find the odd int.

---
+++ Find the odd int.py
@@ -0,0 +1,10 @@
+"""
+Challenge: Given an array, find the int that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+"""
+def find_it(seq):
+	for num in seq:
+		if seq.count(num)%2==1:
+			return num
+	return None
