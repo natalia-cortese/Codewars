27607b5e0166c3910e655ecaf898276794b8930a
Nats <taku246@gmail.com>
2018-10-31 17:09:53 -0300

    simple approach
    check this solution

--- Array diff.py
+++ Array diff.py
@@ -4,7 +4,4 @@ Challenge: Your goal in this kata is to implement an difference function, which 
 It should remove all values from list a, which are present in list b.
 """
 def array_diff(a, b):
-    for num in b:
-    	if num in a:
-    		a=[x for x in a if x!=num]
-    return a
+    return [x for x in a if x not in b]
